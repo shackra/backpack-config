7d21b1dc2afa16f553f34cde19c7d11b59b13791
Jorge Javier Araya Navarro <jorge@esavara.cr>
2026-04-25 21:41:36 -0600

    Registra valores seguros para dirs-local

--- custom.el
+++ custom.el
@@ -4,7 +4,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values '((go-tag-args "-transform" "camelcase"))))
+ '(safe-local-variable-values
+   '((:yaml
+      (:format (:enable t) :validate t :hover t :completion t :schemas
+	       (:https://www.schemastore.org/github-workflow.json
+		["/.github/workflows/*.yml"])
+	       :schemaStore (:enable t)))
+     (org-download-image-dir . "./imagenes")
+     (org-clock-mode-line-total . today)
+     (go-tag-args "-transform" "camelcase"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
